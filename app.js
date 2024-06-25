8cf2abfd14cf7c82498dd4f91284e0fd73a90844
It-4n <128805445+It-4n@users.noreply.github.com>
2024-06-25 07:57:35 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const dotenv = require('dotenv'.config());
+const dotenv = require('dotenv').config();
 const express = require('express');
 const bp = require('body-parser');
 const moment = require('moment');
